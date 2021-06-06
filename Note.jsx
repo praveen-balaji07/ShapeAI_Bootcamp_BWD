6430c82934a655bf7797417ac8aaed0b3702635a
praveen-balaji07 <85355719+praveen-balaji07@users.noreply.github.com>
2021-06-06 02:29:44 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className = "note">
+      <h1>Days of a week:</h1>
+      <u1>
+        <li>Sunday</li>
+        <li>Monday</li>
+        <li>Tuesday</li>
+        <li>Wednesday</li>
+        <li>Thursday</li>
+        <li>Friday</li>
+        <li>Saturday</li>
+      </u1>
+      <p> <h1>Thanks to SHAPEAI</h1> 
+      </p>
+    </div>
+    
+  );
+}
+
+export default Note;
